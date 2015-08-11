836f2adf94dffdcf36d286738974eb694474cf33
Luis Carli <luiscarli@kensho.com>
2015-08-11 16:53:42 -0400

    scaleUtils: mergeRanges, mergeScales, ...

--- src/utils/scaleUtils-test.js
+++ src/utils/scaleUtils-test.js
@@ -1,26 +1,58 @@
 
 import test from 'tape'
 
+import {rectBase} from './rectUtils'
 import * as scaleUtils from './scaleUtils'
 
-test('scaleUtils.scaleForType', t => {
-  t.ok(scaleUtils.scaleForType)
-  t.equal(typeof scaleUtils.scaleForType('linear'), 'function')
-  t.throws(scaleUtils.scaleForType)
+test('scaleUtils.mergeRanges', t => {
+  t.ok(scaleUtils.mergeRanges)
+  t.deepEqual(
+    scaleUtils.mergeRanges({x: {}}, {width: 100}),
+    {x: {range: [0, 100]}}
+  )
+  t.deepEqual(
+    scaleUtils.mergeRanges({y: {}}, {height: 100}),
+    {y: {range: [0, 100]}}
+  )
   t.end()
 })
 
-test('scaleUtils.scaleForDimension', t => {
-  t.ok(scaleUtils.scaleForDimension)
-  t.equal(typeof scaleUtils.scaleForDimension(), 'function')
+test('scaleUtils.getScaleForType', t => {
+  t.ok(scaleUtils.getScaleForType)
+  t.equal(typeof scaleUtils.getScaleForType('linear'), 'function')
+  t.throws(scaleUtils.getScaleForType)
   t.end()
 })
 
-test('scaleUtils.getScales', t => {
-  t.ok(scaleUtils.getScales)
-  t.equal(typeof scaleUtils.getScales({}, {}), 'object')
-  t.equal(typeof scaleUtils.getScales({x: {}}).x, 'function')
-  t.equal(typeof scaleUtils.getScales({y: {}}).y, 'function')
-  t.equal(typeof scaleUtils.getScales({color: {}}).color, 'function')
+test('scaleUtils.getScaleForDimension', t => {
+  t.ok(scaleUtils.getScaleForDimension)
+  t.equal(typeof scaleUtils.getScaleForDimension(scaleUtils.DIMENSION_BASE), 'function')
+  t.end()
+})
+
+test('scaleUtils.mergeScales', t => {
+  t.ok(scaleUtils.mergeScales)
+  t.equal(
+    typeof scaleUtils.mergeScales({x: scaleUtils.DIMENSION_BASE}).x.scale,
+    'function'
+  )
+  t.end()
+})
+
+test('scaleUtils.getRange', t => {
+  t.ok(scaleUtils.getRange)
+  t.deepEqual(
+    scaleUtils.getRange('x', {}, rectBase),
+    [0, 0]
+  )
+  t.deepEqual(
+    scaleUtils.getRange('y', {}, rectBase),
+    [0, 0]
+  )
+  t.deepEqual(
+    scaleUtils.getRange('color', {}, rectBase),
+    scaleUtils.RANGE_LINEAR_COLOR
+  )
+  t.throws(scaleUtils.getRange)
   t.end()
 })
--- src/utils/scaleUtils.js
+++ src/utils/scaleUtils.js
@@ -3,15 +3,15 @@ import R from 'ramda'
 import d3Scale from 'd3-scale'
 import * as rectUtils from './rectUtils'
 
-var dimensionBase = {
+export const DIMENSION_BASE = {
   name: 'dimensionBase',
   type: 'linear',
-  selector: [],
+  path: [undefined],
   domain: [0, 1],
   range: [0, 1],
   // rangeType: 'points' | 'roundPoint' | 'bands' | 'roundBands',
 }
-var SCALE_TYPES = [
+const SCALE_TYPES = [
   'linear',
   'log',
   'ordinal',
@@ -21,37 +21,80 @@ var SCALE_TYPES = [
   'threshold',
   'time',
 ]
-var RANGE_LINEAR_COLOR = ['#ffffd9', '#081d58']
-var RANGE_ORDINAL_COLOR = ['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3']
+export const RANGE_LINEAR_COLOR = ['#ffffd9', '#081d58']
+const RANGE_ORDINAL_COLOR = ['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3']
 
-export function getScales(dimensions = {}, rectInput) {
-  var rect = R.merge(rectUtils.rectBase, rectInput)
-  var scales = {}
-  if (dimensions.x) {
-    scales.x = scaleForDimension(dimensions.x, [rect.x, rectUtils.getMaxX(rect)])
-  }
-  if (dimensions.y) {
-    scales.y = scaleForDimension(dimensions.y, [rect.y, rectUtils.getMaxY(rect)])
-  }
-  if (dimensions.color) {
-    var rangeColor = dimensions.color.type === 'ordinal'
-        ? RANGE_ORDINAL_COLOR
-        : RANGE_LINEAR_COLOR
-    scales.color = scaleForDimension(dimensions.color, rangeColor)
-  }
-  return scales
+/**
+ * Merge default ranges to the dimensions
+ * @param  {Object} dimensions
+ * @param  {Object} rectInput  rect Object
+ * @return {Object}            new dimensions object
+ */
+export function mergeRanges(dimensions, rectInput) {
+  const rect = R.merge(rectUtils.rectBase, rectInput)
+  const mapDimensions = R.mapObjIndexed((dimension, key) => {
+    const range = getRange(key, dimension, rect)
+    return R.merge({range}, dimension)
+  })
+  return mapDimensions(dimensions)
 }
 
-export function scaleForDimension(dimensionInput, rangeInput) {
-  var rangeObj = rangeInput ? {range: rangeInput} : undefined
-  var dimension = R.mergeAll([dimensionBase, rangeObj, dimensionInput])
-  var scale = scaleForType(dimension.type)
+/**
+ * Merge new scales to the dimensions object
+ * @param  {Object} dimensions
+ * @return {Object}            new dimensions object
+ */
+export function mergeScales(dimensions) {
+  return R.mapObj(dimension => {
+    const scale = getScaleForDimension(dimension)
+    return R.merge(dimension, {scale})
+  }, dimensions)
+}
+
+/**
+ * Get new scale according to the dimension options
+ * @param  {Object} dimension
+ * @return {function}           new d3 scale
+ */
+export function getScaleForDimension(dimension) {
+  var scale = getScaleForType(dimension.type)
     .domain(dimension.domain)
     .range(dimension.range)
   return scale
 }
 
-export function scaleForType(type) {
+/**
+ * Get default range according to options
+ * @param  {string} key       dimension name
+ * @param  {Object} dimension
+ * @param  {Object} rect
+ * @return {Array}           range array
+ */
+export function getRange(key, dimension, rect) {
+  switch (key) {
+  case 'x':
+    return [rect.x, rectUtils.getMaxX(rect)]
+  case 'y':
+    return [rect.y, rectUtils.getMaxY(rect)]
+  case 'color':
+    switch (dimension.type) {
+    case 'ordinal':
+      return RANGE_ORDINAL_COLOR
+    default:
+      return RANGE_LINEAR_COLOR
+    }
+    break
+  default:
+    throw new Error('scaleUtils.getRange: invalid key')
+  }
+}
+
+/**
+ * Return a new d3 scale according to the input type
+ * @param  {string} type The string needs to be one of the SCALE_TYPES: ['linear', 'log', 'ordinal', 'pow', 'quantile', 'quantize', 'threshold', 'time' ]
+ * @return {function}      return a new d3 scale
+ */
+export function getScaleForType(type) {
   if (R.contains(type, SCALE_TYPES)) return d3Scale[type]()
-  throw new Error('invalid scale type')
+  throw new Error('scaleUtils.getScaleForType: invalid scale type')
 }
