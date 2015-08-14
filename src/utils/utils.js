21f7457f54b72b36433d1f3c7c9cffb72e7e8728
Luis Carli <luiscarli@kensho.com>
2015-08-14 17:05:56 -0400

    utils: object wrapper for all utils

---
+++ src/utils/utils.js
@@ -0,0 +1,14 @@
+
+import * as canvas from './canvasUtils'
+import * as map from './dataMapUtils'
+import * as dim from './dimensionUtils'
+import * as rect from './rectUtils'
+import * as ticks from './ticksUtils'
+
+export default {
+  canvas,
+  map,
+  dim,
+  rect,
+  ticks,
+}
