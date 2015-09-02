e240ddef6f68fcb485afc89e02ebe5e2eacbf512
Luis Carli <luiscarli@kensho.com>
2015-09-02 17:32:49 -0400

    del dataMapUtils, dimensionUtils, ticksUtils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,12 @@
 
 import * as canvas from './canvasUtils'
-import * as dim from './dimensionUtils'
-import * as map from './dataMapUtils'
 import * as rect from './rectUtils'
-import * as ticks from './ticksUtils'
+import * as vis from './visUtils'
 import path from './path'
 
 export default {
   canvas,
-  dim,
-  map,
   path,
   rect,
-  ticks,
+  vis,
 }
