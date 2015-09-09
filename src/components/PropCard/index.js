f1269a0567e7dadd3e601e5b107321e0788f6515
Luis Carli <luiscarli@kensho.com>
2015-09-09 17:40:48 -0400

    PropCard: convert to styleVars

--- src/components/PropCard/index.js
+++ src/components/PropCard/index.js
@@ -2,14 +2,18 @@
 import React, {PropTypes} from 'react'
 import {DragSource} from 'react-dnd'
 
-const styles = {
-  item: {
-    background: 'hsl(0, 0%, 90%)',
-    fontSize: 15,
-    margin: 5,
-    padding: 5,
-    cursor: 'pointer',
-  },
+import defaultStyleVars from '../styleVars'
+
+export function getStyles(styleVars) {
+  return {
+    item: {
+      background: 'hsl(0, 0%, 90%)',
+      fontSize: styleVars.fontSize,
+      margin: 5,
+      padding: 5,
+      cursor: 'pointer',
+    },
+  }
 }
 
 const cardSource = {
@@ -26,18 +30,24 @@ function collect(connect) {
   }
 }
 
+/**
+ * Component responsible for the draggable cards on list of data properties (DataList)
+ */
 export const PropCard = React.createClass({
   displayName: 'PropCard',
   propTypes: {
     connectDragSource: PropTypes.func,
     isDragging: PropTypes.bool,
+    styleVars: PropTypes.object,
     text: PropTypes.string,
   },
   getDefaultProps() {
     return {
+      styleVars: {...defaultStyleVars},
     }
   },
   render() {
+    const styles = getStyles(this.props.styleVars)
     const { isDragging, connectDragSource, text } = this.props
     return connectDragSource(
       <div style={{...styles.item, opacity: isDragging ? 0.5 : 1 }}>
