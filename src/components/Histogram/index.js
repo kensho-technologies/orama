447642ebd99b7bc801d8988e069ecacad4354e8a
Luis Carli <luiscarli@kensho.com>
2015-09-09 17:37:03 -0400

    Histogram: component first version

---
+++ src/components/Histogram/index.js
@@ -0,0 +1,102 @@
+
+import React, {PropTypes} from 'react'
+import R from 'ramda'
+import d3 from 'd3'
+
+import utils from '../../utils'
+
+import CanvasRender from '../CanvasRender'
+import ChartInput from '../ChartInput'
+
+// export function getStyles() {
+//   return {
+//   }
+// }
+
+export default React.createClass({
+  displayName: 'Histogram',
+  propTypes: {
+    data: PropTypes.array,
+    margin: PropTypes.object,
+    size: PropTypes.object,
+    styleVars: PropTypes.object,
+    xName: PropTypes.string,
+    xProp: PropTypes.string,
+  },
+  getDefaultProps() {
+    return {
+      data: [],
+      margin: {
+        left: 5, right: 5,
+        top: 5, bottom: 5,
+      },
+      size: {width: 500, height: 400},
+    }
+  },
+  render() {
+    // const styles = getStyles(this.props.styleVars)
+    const {data, xProp, size, margin} = this.props
+
+    const plotRect = utils.rect.marginInset(margin, size)
+
+    const xType = utils.vis.getType(data, xProp)
+    const xDomain = utils.vis.getDomain(data, xProp, xType)
+    const xRange = utils.vis.getRange('x', plotRect, xType)
+    const xTickCount = 20
+    const xScale = utils.vis.getScale('x', xType, xDomain, xRange, xTickCount)
+
+    const histogramLayout = d3.layout.histogram()
+      .value(R.prop(xProp))
+      .bins(20)
+    const histogramData = histogramLayout(data)
+
+    const yProp = 'y'
+    const yType = 'linear'
+    const yDomain = utils.vis.getDomain(histogramData, yProp, yType)
+    const yRange = utils.vis.getRange('y', plotRect, yType)
+    const yScale = utils.vis.getScale('y', yType, yDomain, yRange)
+    const yMap = utils.vis.getMap(yProp, yScale)
+
+    const xMap = utils.vis.getMap('x', xScale)
+
+    const renderData = R.map(pointD => {
+      const x = xMap(pointD)
+      const y = yMap(pointD)
+      const path2D = utils.path()
+      path2D.rect(x, y, plotRect.width / 20 - 2, size.height - y)
+      return {
+        label: `${pointD.x}, ${pointD.y}`,
+        tooltip: [],
+        path2D,
+        raw: pointD,
+        type: 'area',
+        x,
+        y,
+      }
+    }, histogramData)
+
+    const containerStyle = {
+      height: this.props.size.height,
+      position: 'relative',
+      width: this.props.size.width,
+    }
+    if (xType !== 'linear') return null
+    return (
+      <div>
+        <div style={containerStyle}>
+          <CanvasRender
+            plotRect={plotRect}
+            renderData={renderData}
+            size={this.props.size}
+            styleVars={this.props.styleVars}
+          />
+          <ChartInput
+            plotRect={plotRect}
+            renderData={renderData}
+            size={this.props.size}
+          />
+        </div>
+      </div>
+    )
+  },
+})
