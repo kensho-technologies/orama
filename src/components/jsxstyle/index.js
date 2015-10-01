6f83cc38a260e1440aaf121f53ae849332b0a834
Luis Carli <luiscarli@kensho.com>
2015-10-01 17:14:49 -0400

    jsxstyle: add methods documentation

--- src/components/jsxstyle/index.js
+++ src/components/jsxstyle/index.js
@@ -13,6 +13,14 @@ const cssDisplayNames = {
   Inline: 'inline',
 }
 
+/**
+ * Exports an object containing a React component for each cssDisplayName
+ * The components have a CSS display according to their names and convert their props
+ * into autoprefixed CSS styles
+ *
+ * @example
+ * <Block margin={10}/> // <div style="display: block; margin: 10px;"/>
+ */
 export default R.mapObjIndexed((display, displayName) => {
   return makeStyleComponentClass(display, displayName)
 }, cssDisplayNames)
--- src/components/jsxstyle/methods.js
+++ src/components/jsxstyle/methods.js
@@ -3,8 +3,19 @@ import R from 'ramda'
 
 import * as prefixers from './prefixers'
 
+/**
+ * Creates function for pipeing a style through all local prefixers
+ * @param  {object} style
+ * @return {object}          prefixed style object
+ */
 export const autoprefix = R.pipe(...R.values(prefixers))
 
+/**
+ * Returns a React component that converts props into autoprefixed style, with a specific display value
+ * @param  {string} display     css display value
+ * @param  {string} displayName component displayName value
+ * @return {ReactComponent}
+ */
 export function makeStyleComponentClass(display, displayName) {
   return React.createClass({
     displayName: displayName,
