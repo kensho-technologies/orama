931321102e7929c6c8c7a5c94af57f2067c5d550
Luis Carli <luiscarli@kensho.com>
2015-10-22 14:14:07 -0400

    TextRender: add propTypes and defaultProps

--- src/components/TextRender/index.js
+++ src/components/TextRender/index.js
@@ -1,53 +1,54 @@
 
-import React from 'react'
+import React, {PropTypes} from 'react'
 import R from 'ramda'
 
 import {Block} from '../Display'
 
 import defaultTheme from '../defaultTheme'
-import pureHOC from '../../utils/pureHOC'
 
 const mapIndexed = R.addIndex(R.map)
-
-/**
- * Returns text elements according to the renderTextData
- */
-const getTextElements = mapIndexed((d, i) => (
-  <Block
-    background='hsla(0, 0%, 97%, 0.6)'
-    display='block'
-    key={i}
-    left={d.x}
-    padding={2}
-    pointerEvents='fill'
-    position='absolute'
-    textAlign={d.textAlign}
-    top={d.y}
-  >
-    {d.text}
-  </Block>
-))
 
 /**
  * Component to render text data.
  * This component is used inside of the ChartRender, and it's sibling with the CanvasRender, CanvasHoverRender and friends.
  */
-const TextRender = ({
-  renderTextData = [],
-  size,
-  theme = defaultTheme,
-}) => (
+const TextRender = props => (
   <Block
-    fontFamily={theme.font}
-    fontSize={theme.fontSize}
+    fontFamily={props.theme.font}
+    fontSize={props.theme.fontSize}
     fontWeight='bold'
-    height={size.height}
+    height={props.size.height}
     pointerEvents='none'
     position='absolute'
-    width={size.width}
+    width={props.size.width}
   >
-    {getTextElements(renderTextData)}
+    {mapIndexed((d, i) => (
+      <Block
+        background='hsla(0, 0%, 97%, 0.6)'
+        display='block'
+        key={i}
+        left={d.x}
+        padding={2}
+        pointerEvents='fill'
+        position='absolute'
+        textAlign={d.textAlign}
+        top={d.y}
+      >
+        {d.text}
+      </Block>
+    ), props.renderTextData)}
   </Block>
 )
 
-export default pureHOC(TextRender)
+TextRender.propTypes = {
+  renderTextData: PropTypes.array,
+  size: PropTypes.object,
+  theme: PropTypes.object,
+}
+
+TextRender.defaultProps = {
+  renderTextData: [],
+  theme: defaultTheme,
+}
+
+export default TextRender
