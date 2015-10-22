5fa4c8792feb1a124033b282bbe14a22cb538bc7
Luis Carli <luiscarli@kensho.com>
2015-10-22 10:47:23 -0400

    TextRender: stateless functions

--- src/components/TextRender/index.js
+++ src/components/TextRender/index.js
@@ -1,60 +1,53 @@
 
-import React, {PropTypes} from 'react'
+import React from 'react'
 import R from 'ramda'
 
 import {Block} from '../Display'
 
-import shouldPureComponentUpdate from 'react-pure-render/function'
 import defaultTheme from '../defaultTheme'
+import pureHOC from '../../utils/pureHOC'
 
 const mapIndexed = R.addIndex(R.map)
 
-const getTextElements = (props) => (
-  mapIndexed((d, i) => (
-    <Block
-      background='hsla(0, 0%, 97%, 0.6)'
-      display='block'
-      key={i}
-      left={d.x}
-      padding={2}
-      position='absolute'
-      textAlign={d.textAlign}
-      top={d.y}
-      pointerEvents='fill'
-    >
-      {d.text}
-    </Block>
-  ), props.renderTextData)
+/**
+ * Returns text elements according to the renderTextData
+ */
+const getTextElements = mapIndexed((d, i) => (
+  <Block
+    background='hsla(0, 0%, 97%, 0.6)'
+    display='block'
+    key={i}
+    left={d.x}
+    padding={2}
+    pointerEvents='fill'
+    position='absolute'
+    textAlign={d.textAlign}
+    top={d.y}
+  >
+    {d.text}
+  </Block>
+))
+
+/**
+ * Component to render text data.
+ * This component is used inside of the ChartRender, and it's sibling with the CanvasRender, CanvasHoverRender and friends.
+ */
+const TextRender = ({
+  renderTextData = [],
+  size,
+  theme = defaultTheme,
+}) => (
+  <Block
+    fontFamily={theme.font}
+    fontSize={theme.fontSize}
+    fontWeight='bold'
+    height={size.height}
+    pointerEvents='none'
+    position='absolute'
+    width={size.width}
+  >
+    {getTextElements(renderTextData)}
+  </Block>
 )
 
-export default React.createClass({
-  displayName: 'TextRender',
-  propTypes: {
-    renderTextData: PropTypes.array,
-    size: PropTypes.object,
-    theme: PropTypes.object,
-  },
-  getDefaultProps() {
-    return {
-      renderTextData: [],
-      theme: {...defaultTheme},
-    }
-  },
-  shouldComponentUpdate: shouldPureComponentUpdate,
-  render() {
-    return (
-      <Block
-        display='block'
-        fontFamily={this.props.theme.font}
-        fontSize={this.props.theme.fontSize}
-        fontWeight='bold'
-        height={this.props.size.height}
-        pointerEvents='none'
-        position='absolute'
-        width={this.props.size.width}
-      >
-        {getTextElements(this.props)}
-      </Block>
-    )
-  },
-})
+export default pureHOC(TextRender)
