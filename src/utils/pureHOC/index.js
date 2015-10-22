dab86c055b02ed8b3c9ec00ba63347f75a1eee8d
Luis Carli <luiscarli@kensho.com>
2015-10-22 14:30:21 -0400

    pureHOC: deal with propTypes and defaultProps

--- src/utils/pureHOC/index.js
+++ src/utils/pureHOC/index.js
@@ -3,7 +3,11 @@ import React from 'react'
 import shouldPureComponentUpdate from 'react-pure-render/function'
 
 const pureHOC = (Child) => React.createClass({
-  displayName: `${Child.name}(Pure)`,
+  displayName: Child.name,
+  propTypes: Child.propTypes,
+  getDefaultProps() {
+    return Child.defaultProps
+  },
   shouldComponentUpdate: shouldPureComponentUpdate,
   render() {
     return Child(this.props)
