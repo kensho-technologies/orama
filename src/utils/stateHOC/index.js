4583adbc61d444d0714285fa60ea2e0756e0e67a
Luis Carli <luiscarli@kensho.com>
2015-10-26 10:49:19 -0400

    stateHOC: handle both class components and stateless func components

--- src/utils/stateHOC/index.js
+++ src/utils/stateHOC/index.js
@@ -1,5 +1,13 @@
+/* eslint react/no-multi-comp:0 */
 
 import React from 'react'
+
+const isReferentiallyTransparentFunctionComponent = Component => (
+  Component &&
+  typeof Component !== 'string' &&
+  !(Component.prototype && Component.prototype.render) &&
+  !Component.contextTypes
+)
 
 /**
  * Funtion for wrapping 'stateless functional components' during development, allowing then to respond to onUpdate without changes to parent components.
@@ -20,28 +28,57 @@ import React from 'react'
  *
  * export default stateHOC(StatelessFunctionalComponent)
  */
-const stateHOC = (Child, initialState = {}) => React.createClass({
-  displayName: `${Child.name}(state)`,
-  propTypes: Child.propTypes,
-  getDefaultProps() {
-    return Child.defaultProps
-  },
-  getInitialState() {
-    return initialState
-  },
-  componentWillReceiveProps() {
-    this.setState(initialState)
-  },
-  handleChildUpdate(childProps) {
-    this.setState(childProps)
-  },
-  render() {
-    return Child({
-      ...this.props,
-      ...this.state,
-      onUpdate: this.handleChildUpdate,
+const stateHOC = (Child, initialState = {}) => {
+  if (isReferentiallyTransparentFunctionComponent(Child)) {
+    return React.createClass({
+      displayName: `${Child.name}(state)`,
+      propTypes: Child.propTypes,
+      getDefaultProps() {
+        return Child.defaultProps
+      },
+      getInitialState() {
+        return initialState
+      },
+      componentWillReceiveProps() {
+        this.setState(initialState)
+      },
+      handleChildUpdate(childProps) {
+        this.setState(childProps)
+      },
+      render() {
+        return Child({
+          ...this.props,
+          ...this.state,
+          onUpdate: this.handleChildUpdate,
+        })
+      },
     })
-  },
-})
+  }
+
+  return React.createClass({
+    displayName: `${Child.displayName}(state)`,
+    getDefaultProps() {
+      return {}
+    },
+    getInitialState() {
+      return initialState
+    },
+    componentWillReceiveProps() {
+      this.setState(initialState)
+    },
+    handleChildUpdate(childProps) {
+      this.setState(childProps)
+    },
+    render() {
+      return (
+        <Child
+          {...this.props}
+          {...this.state}
+          onUpdate={this.handleChildUpdate}
+        />
+      )
+    },
+  })
+}
 
 export default stateHOC
