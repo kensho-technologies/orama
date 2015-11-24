2553f971a50e5d6d482f64f37126d42c90c71775
Luis Carli <luiscarli@kensho.com>
2015-11-23 23:48:00 -0500

    areas: extract and plot x0

--- src/Chart2/plotFunctions/index.js
+++ src/Chart2/plotFunctions/index.js
@@ -2,35 +2,7 @@
 import _ from 'lodash'
 import utils from '../../utils'
 import {plotValue} from '../plotValue'
-import {points} from '../points'
 
-export const areas = props => {
-  if (!props.xMap || !props.yMap) return undefined
-  const {data} = props
-  const path2D = utils.path()
-  path2D.moveTo(
-    plotValue(props, 'x', _.first(data)),
-    plotValue(props, 'y', _.first(data))
-  )
-  _.each(data, d => {
-    path2D.lineTo(
-      plotValue(props, 'x', d),
-      plotValue(props, 'y', d)
-    )
-  })
-  _.eachRight(data, d => {
-    path2D.lineTo(
-      plotValue(props, 'x', d),
-      props.yScale(0),
-    )
-  })
-  const areaRender = {
-    type: 'area',
-    path2D,
-  }
-  const pointData = points(props)
-  return [].concat(areaRender, pointData)
-}
 export const barsDataMap = (props, d) => {
   const path2D = utils.path()
   const x = plotValue(props, 'x', d, props.plotRect.x)
