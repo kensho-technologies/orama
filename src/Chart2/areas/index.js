a440941ee78397771832afb8fc41cf8a24d8fa09
Luis Carli <luiscarli@kensho.com>
2015-11-24 11:34:57 -0500

    areas: infer orientation, handle multi areas

--- src/Chart2/areas/index.js
+++ src/Chart2/areas/index.js
@@ -4,9 +4,7 @@ import utils from '../../utils'
 import {plotValue} from '../plotValue'
 import {points} from '../points'
 
-export const areas = props => {
-  if (!props.xMap || !props.yMap) return undefined
-  const {data} = props
+export const getArea = (props, data) => {
   const path2D = utils.path()
   path2D.moveTo(
     plotValue(props, _.first(data), 'x'),
@@ -18,16 +16,41 @@ export const areas = props => {
       plotValue(props, d, 'y')
     )
   })
-  _.eachRight(data, d => {
+  // if there's no base position accessors
+  if (!props.y0 && !props.x0) {
     path2D.lineTo(
-      plotValue(props, d, 'x'),
-      plotValue(props, d, 'y0'),
+      plotValue(props, _.last(data), 'x'),
+      props.yScale(0),
     )
-  })
+    path2D.lineTo(
+      plotValue(props, _.first(data), 'x'),
+      props.yScale(0),
+    )
+  // if the base is on the y axis
+  } else if (props.y0 && !props.x0) {
+    _.eachRight(data, d => {
+      path2D.lineTo(
+        plotValue(props, d, 'x'),
+        plotValue(props, d, 'y0'),
+      )
+    })
+  // if the base is on the x axis
+  } else if (!props.y0 && props.x0) {
+    _.eachRight(data, d => {
+      path2D.lineTo(
+        plotValue(props, d, 'x0'),
+        plotValue(props, d, 'y'),
+      )
+    })
+  }
   path2D.closePath()
+  const fill = plotValue(props, _.first(data), 'fill')
+  const alpha = plotValue(props, _.first(data), 'alpha')
   const areaRender = {
-    type: 'area',
+    alpha,
+    fill,
     path2D,
+    type: 'area',
   }
   const pointData = points(props)
   const pointData0 = points({
@@ -35,4 +58,15 @@ export const areas = props => {
     y: props.y0,
   })
   return [].concat(areaRender, pointData, pointData0)
+}
+export const areas = props => {
+  if (!props.xMap || !props.yMap) return undefined
+  if (_.isArray(_.first(props.data))) {
+    return _.reduce(
+      props.data,
+      (acc, data) => acc.concat(getArea(props, data)),
+      []
+    )
+  }
+  return getArea(props, props.data)
 }
