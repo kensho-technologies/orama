6b427a1716bd6fd69b7b6d0b3d8fc440c554db03
Luis Carli <luiscarli@kensho.com>
2015-11-30 14:59:42 -0500

    isStatelessComponentFunction: delete unused code

--- src/utils/isStatelessComponentFunction/index.js
+++
@@ -1,9 +0,0 @@
-
-const isStatelessComponentFunction = Component => (
-  Component &&
-  typeof Component !== 'string' &&
-  !(Component.prototype && Component.prototype.render) &&
-  !Component.contextTypes
-)
-
-export default isStatelessComponentFunction
