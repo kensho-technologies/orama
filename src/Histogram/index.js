b22b4dc8a41073c1362fc4f48d3a268aa5b03a8b
Luis Carli <luiscarli@kensho.com>
2015-11-30 11:38:11 -0500

    Histogram: use new Chart component

--- src/Histogram/index.js
+++ src/Histogram/index.js
@@ -1,14 +1,11 @@
 
 import React, {PropTypes} from 'react'
-import R from 'ramda'
+import _ from 'lodash'
 import d3 from 'd3'
-
 import utils from '../utils'
 
-import {Block} from 'react-display'
-import CanvasRender from '../CanvasRender'
-import CanvasInput from '../CanvasInput'
-
+import Chart2 from '../Chart2'
+import {bars} from '../Chart2/bars'
 /**
  * Renders a Histogram using the same logic as `Chart`.
  * This component have not been generalized yet.
@@ -42,67 +39,37 @@ export default React.createClass({
     }
   },
   render() {
-    const {data, xProp, size, margin} = this.props
-
-    const plotRect = utils.rect.marginInset(margin, size)
-
+    const {data, xProp, size} = this.props
     const xType = this.props.xType ||
       utils.vis.getType(data, xProp)
-    const xDomain = utils.vis.getDomain(data, xProp, xType)
-    const xRange = utils.vis.getRange('x', plotRect, xType)
-    const xTickCount = 20
-    const xScale = utils.vis.getScale('x', xType, xDomain, xRange, xTickCount)
-
     const histogramLayout = d3.layout.histogram()
-      .value(R.prop(xProp))
+      .value(d => _.get(d, xProp))
       .bins(20)
-    const histogramData = histogramLayout(data)
-
-    const yProp = 'y'
-    const yType = 'linear'
-    const yDomain = utils.vis.getDomain(histogramData, yProp, yType)
-    const yRange = utils.vis.getRange('y', plotRect, yType)
-    const yScale = utils.vis.getScale('y', yType, yDomain, yRange)
-    const yMap = utils.vis.getMap(yProp, yScale)
-
-    const xMap = utils.vis.getMap('x', xScale)
-
-    const renderData = R.map(pointD => {
-      const x = xMap(pointD)
-      const y = yMap(pointD)
-      const path2D = utils.path()
-      path2D.rect(x, y, plotRect.width / 20 + 1, size.height - y)
-      return {
-        label: `${pointD.x}, ${pointD.y}`,
-        tooltip: [],
-        path2D,
-        raw: pointD,
-        type: 'area',
-        x,
-        y,
-      }
-    }, histogramData)
-
+    const histogramData = _.map(
+      histogramLayout(data),
+      d => ({
+        x1: d.x,
+        x2: d.x + d.dx,
+        y: d.y,
+      })
+    )
     if (xType !== 'linear') return null
     return (
-      <Block
-        height={this.props.size.height}
-        position={'relative'}
-        width={this.props.size.width}
-      >
-        <CanvasRender
-          plotRect={plotRect}
-          renderData={renderData}
-          size={this.props.size}
-          theme={this.props.theme}
-        />
-        <CanvasInput
-          plotRect={plotRect}
-          renderData={renderData}
-          size={this.props.size}
-          theme={this.props.theme}
-        />
-      </Block>
+      <Chart2
+        backgroundOffset={0}
+        backgroundShow={false}
+        barsGutter={0}
+        data={histogramData}
+        margin={{left: 0, top: 0, bottom: 0, right: 0}}
+        plot={bars}
+        size={size}
+        x1='x1'
+        x2='x2'
+        xShowGuides={false}
+        y='y'
+        yName='frequency'
+        yShowGuides={false}
+      />
     )
   },
 })
