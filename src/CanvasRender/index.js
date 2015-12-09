9bb9084e4c23c169df3ed64f2b3ad7cb2fe859b2
Luis Carli <luiscarli@kensho.com>
2015-12-09 13:54:28 -0500

    CanvasRender: retina render

--- src/CanvasRender/index.js
+++ src/CanvasRender/index.js
@@ -2,6 +2,7 @@
 import React, {PropTypes} from 'react'
 import {DEFAULT_THEME} from '../defaultTheme'
 import {basicRender} from './basicRender'
+import {stateHOC} from 'on-update'
 
 /**
  * Component create a Canvas and use the renderData for drawing geometries on it.
@@ -11,30 +12,36 @@ import {basicRender} from './basicRender'
 const handleCanvasRef = (props, canvas) => {
   if (canvas) {
     const ctx = canvas.getContext('2d')
+    ctx.save()
+    ctx.scale(2, 2)
     props.render(props, ctx)
+    ctx.restore()
   }
 }
-export const CanvasRender = props => (
+export const _CanvasRender = props => (
   <canvas
-    height={props.size.height}
+    height={props.size.height * 2}
     ref={canvas => handleCanvasRef(props, canvas)}
     style={{
-      position: 'absolute',
       display: 'block',
+      height: props.size.height,
+      position: 'absolute',
+      width: props.size.width,
     }}
-    width={props.size.width}
+    width={props.size.width * 2}
   />
 )
-CanvasRender.propTypes = {
+_CanvasRender.propTypes = {
   clip: PropTypes.bool,
   plotRect: PropTypes.object,
   render: PropTypes.func.isRequired,
   renderData: PropTypes.array,
   size: PropTypes.object.isRequired,
 }
-CanvasRender.defaultProps = {
+_CanvasRender.defaultProps = {
   render: basicRender,
   renderData: [],
   size: {width: 0, height: 0},
   theme: DEFAULT_THEME,
 }
+export const CanvasRender = stateHOC(_CanvasRender)
