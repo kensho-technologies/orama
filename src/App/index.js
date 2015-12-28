de26d23e1aa4d79b70147628eb1c650d1898fbdc
Luis Carli <luiscarli@kensho.com>
2015-12-28 18:35:38 -0500

    App: use component layers API

--- src/App/index.js
+++ src/App/index.js
@@ -4,7 +4,14 @@ import _ from 'lodash'
 import {utcFormat} from 'd3-time-format'
 
 import {Main, Block, Row} from 'react-display'
+import {Areas} from '../Layer'
+import {Bars} from '../Layer'
 import {Chart} from '../Chart'
+import {Guides} from '../Layer'
+import {Lines} from '../Layer'
+import {Points} from '../Layer'
+import {Ranges} from '../Layer'
+import {Texts} from '../Layer'
 import * as plots from '../plots'
 
 export const TextBody = props => (
@@ -41,220 +48,231 @@ export const App = props => (
     <TextBody>
       <ChartMargin>
         <Chart // scaterplot
-          alphaValue={0.3}
-          data={[props.appl, props.fb]}
-          fill='Name'
           height={300}
-          label='Name'
-          radiusValue={2}
-          tooltipExtraDimensions={['Date']}
-          tooltipKeys={['x', 'y']}
-          x='Open'
-          y='Volume'
           yType='log'
-        />
+        >
+          <Points
+            alphaValue={0.3}
+            data={[props.appl, props.fb]}
+            fill='Name'
+            label='Name'
+            radiusValue={2}
+            tooltipExtraDimensions={['Date']}
+            tooltipKeys={['x', 'y']}
+            x='Open'
+            y='Volume'
+          />
+        </Chart>
       </ChartMargin>
       <ChartMargin>
         <Chart // multi line chart
-          data={[props.appl, props.fb]}
           height={300}
-          label='Name'
-          layers={[
-            {
-              data: [
-                {Date: new Date(2010, 5), Close: 80},
-                {Date: new Date(2010, 6), Close: 90},
-                {Date: new Date(2010, 7), Close: 60},
-              ],
-              plot: plots.points,
-              radiusValue: 3,
-              skipExtractArrays: true,
-              x: 'Date',
-              y: 'Close',
-            },
-            {
-              data: [
-                {
-                  value: 'Text Plot', Date: new Date(2004, 1),
-                  Close: 150, textAlign: 'center',
-                },
-              ],
-              x: 'Date',
-              y: 'Close',
-              label: 'value',
-              plot: plots.texts,
-            },
-          ]}
-          plot={plots.lines}
-          tooltipKeys={['x', 'y']}
-          x='Date'
-          xTickFormat={utcFormat('%Y')}
-          y='Close'
-        />
+        >
+          <Lines
+            data={[props.appl, props.fb]}
+            label='Name'
+            tooltipKeys={['x', 'y']}
+            x='Date'
+            xTickFormat={utcFormat('%Y')}
+            y='Close'
+          />
+          <Points
+            data={[
+              {Date: new Date(2010, 5), Close: 80},
+              {Date: new Date(2010, 6), Close: 90},
+              {Date: new Date(2010, 7), Close: 60},
+            ]}
+            plot={plots.points}
+            radiusValue={3}
+            skipExtractArrays={true}
+            x='Date'
+            y='Close'
+          />
+          <Texts
+            data={[
+              {
+                value: 'Text Plot', Date: new Date(2004, 1),
+                Close: 150, textAlign: 'center',
+              },
+            ]}
+            label='value'
+            plot={plots.texts}
+            x='Date'
+            y='Close'
+          />
+        </Chart>
       </ChartMargin>
       <ChartMargin>
         <Chart // multi area chart y0
-          data={[
-            _.filter(props.appl, d => d.Date.getFullYear() > 2011),
-            _.filter(props.fb, d => d.Date.getFullYear() > 2011),
-          ]}
-          fill='Name'
           height={300}
-          plot={plots.areas}
-          x='Date'
-          xTickFormat={utcFormat('%b %y\'')}
-          y='High'
-          y0='Low'
-        />
+        >
+          <Areas
+            data={[
+              _.filter(props.appl, d => d.Date.getFullYear() > 2011),
+              _.filter(props.fb, d => d.Date.getFullYear() > 2011),
+            ]}
+            fill='Name'
+            x='Date'
+            xTickFormat={utcFormat('%b %y\'')}
+            y='High'
+            y0='Low'
+          />
+        </Chart>
       </ChartMargin>
       <ChartMargin>
         <Chart // vertical bar chart
-          data={[
-            {Name: 'APPL', value: 50},
-            {Name: 'FB', value: 150},
-            {Name: 'GOOGL', value: 10},
-          ]}
           height={300}
-          plot={plots.bars}
-          x='Name'
           xShowGuides={false}
-          y='value'
           yZeroBased={true}
-        />
+        >
+          <Bars
+            data={[
+              {Name: 'APPL', value: 50},
+              {Name: 'FB', value: 150},
+              {Name: 'GOOGL', value: 10},
+            ]}
+            x='Name'
+            y='value'
+          />
+        </Chart>
       </ChartMargin>
       <ChartMargin>
         <Chart // horizontal bar chart
           backgroundOffset={1}
-          data={[
-            {Name: 'APPL', value: 50},
-            {Name: 'FB', value: 150},
-            {Name: 'GOOGL', value: 10},
-            {Name: 'APPL2', value: 80},
-            {Name: 'FB2', value: 100},
-            {Name: 'GOOGL2', value: 0},
-          ]}
           height={150}
-          plot={plots.bars}
-          x='value'
           xZeroBased={true}
-          y='Name'
           yShowGuides={false}
-        />
-      </ChartMargin>
-      <ChartMargin>
-        <Chart // small multiline
-          height={300}
-          layers={[
-            {
-              data: [{x1: 4, x2: 5.2}, {y1: 125, y2: 100}],
-              tooltipExtraDimensions: ['x1', 'x2', 'y1', 'y2'],
-              tooltipShowKeys: false,
-              fillValue: 'hsl(0, 0%, 93%)',
-              plot: plots.ranges,
-              showHover: false,
-            },
-            {
-              data: [{x: 3.5}, {y: 125}],
-              tooltipExtraDimensions: ['x', 'y'],
-              tooltipShowKeys: false,
-              strokeValue: 'red',
-              plot: plots.guides,
-            },
-            {
-              data: [
-                [{Name: 1, value: 50},
-                {Name: 2, value: 150},
-                {Name: 3, value: 10},
-                {Name: 4, value: 80},
-                {Name: 5, value: 100},
-                {Name: 6, value: 0}],
-                [{Name: 1, value: 32},
-                {Name: 2, value: 160},
-                {Name: 3, value: 20},
-                {Name: 4, value: 40},
-                {Name: 5, value: 150},
-                {Name: 6, value: 10}],
-              ],
-              lineDashValue: [8],
-              plot: plots.lines,
-              x: 'Name',
-              xShowGuides: false,
-              y: 'value',
-              yZeroBased: true,
-            },
-          ]}
-        />
-      </ChartMargin>
-      <ChartMargin>
-        <Chart // vertical bar chart
-          data={[
-            {x1: -10, x2: -5, value: 50},
-            {x1: -5, x2: 0, value: 150},
-            {x1: 0, x2: 5, value: 100},
-            {x1: 5, x2: 10, value: 10},
-          ]}
-          height={300}
-          plot={plots.bars}
-          x1='x1'
-          x2='x2'
-          xShowGuides={false}
-          y='value'
-          yZeroBased={true}
-        />
-      </ChartMargin>
-      <ChartMargin>
-        <Chart // small area
-          data={[
-            {Name: 1, value: 50},
-            {Name: 2, value: 150},
-            {Name: 3, value: 10},
-            {Name: 4, value: 80},
-            {Name: 5, value: 100},
-            {Name: 6, value: 0},
-          ]}
-          height={300}
-          plot={plots.areas}
-          x='Name'
-          xShowGuides={false}
-          y='value'
-          yZeroBased={true}
-        />
-      </ChartMargin>
-      <ChartMargin>
-        <Chart // ordinal line and bars
-          backgroundOffset={1}
-          data={[
-            {Name: 'APPL', value: 50},
-            {Name: 'FB', value: 150},
-            {Name: 'GOOGL', value: 10},
-            {Name: 'APPL2', value: 80},
-            {Name: 'FB2', value: 100},
-            {Name: 'GOOGL2', value: 0},
-          ]}
-          height={300}
-          label='Name'
-          layers={[{
-            data: [
+        >
+          <Bars
+            data={[
               {Name: 'APPL', value: 50},
               {Name: 'FB', value: 150},
               {Name: 'GOOGL', value: 10},
               {Name: 'APPL2', value: 80},
               {Name: 'FB2', value: 100},
               {Name: 'GOOGL2', value: 0},
-            ],
-            plot: plots.lines,
-            x: 'Name',
-            y: 'value',
-            strokeValue: 'red',
-          }]}
-          plot={plots.bars}
-          tooltipKeys={['y']}
-          tooltipShowKeys={false}
-          x='Name'
+            ]}
+            x='value'
+            y='Name'
+          />
+        </Chart>
+      </ChartMargin>
+      <ChartMargin>
+        <Chart // small multiline
+          height={300}
+        >
+          <Ranges
+            data={[{x1: 4, x2: 5.2}, {y1: 125, y2: 100}]}
+            fillValue='hsl(0, 0%, 93%)'
+            showHover={false}
+            tooltipExtraDimensions={['x1', 'x2', 'y1', 'y2']}
+            tooltipShowKeys={false}
+          />
+          <Guides
+            data={[{x: 3.5}, {y: 125}]}
+            strokeValue='red'
+            tooltipExtraDimensions={['x', 'y']}
+            tooltipShowKeys={false}
+          />
+          <Lines
+            data={[
+              [{Name: 1, value: 50},
+              {Name: 2, value: 150},
+              {Name: 3, value: 10},
+              {Name: 4, value: 80},
+              {Name: 5, value: 100},
+              {Name: 6, value: 0}],
+              [{Name: 1, value: 32},
+              {Name: 2, value: 160},
+              {Name: 3, value: 20},
+              {Name: 4, value: 40},
+              {Name: 5, value: 150},
+              {Name: 6, value: 10}],
+            ]}
+            lineDashValue={[8]}
+            x='Name'
+            xShowGuides={false}
+            y='value'
+            yZeroBased={true}
+          />
+        </Chart>
+      </ChartMargin>
+      <ChartMargin>
+        <Chart // vertical bar chart
+          height={300}
           xShowGuides={false}
-          y='value'
           yZeroBased={true}
-        />
+        >
+          <Bars
+            data={[
+              {x1: -10, x2: -5, value: 50},
+              {x1: -5, x2: 0, value: 150},
+              {x1: 0, x2: 5, value: 100},
+              {x1: 5, x2: 10, value: 10},
+            ]}
+            x1='x1'
+            x2='x2'
+            y='value'
+          />
+        </Chart>
+      </ChartMargin>
+      <ChartMargin>
+        <Chart // small area
+          height={300}
+          xShowGuides={false}
+          yZeroBased={true}
+        >
+          <Areas
+            data={[
+              {Name: 1, value: 50},
+              {Name: 2, value: 150},
+              {Name: 3, value: 10},
+              {Name: 4, value: 80},
+              {Name: 5, value: 100},
+              {Name: 6, value: 0},
+            ]}
+            x='Name'
+            y='value'
+          />
+        </Chart>
+      </ChartMargin>
+      <ChartMargin>
+        <Chart // ordinal line and bars
+          backgroundOffset={1}
+          height={300}
+          xShowGuides={false}
+          yZeroBased={true}
+        >
+          <Bars
+            data={[
+              {Name: 'APPL', value: 50},
+              {Name: 'FB', value: 150},
+              {Name: 'GOOGL', value: 10},
+              {Name: 'APPL2', value: 80},
+              {Name: 'FB2', value: 100},
+              {Name: 'GOOGL2', value: 0},
+            ]}
+            label='Name'
+            tooltipKeys={['y']}
+            tooltipShowKeys={false}
+            x='Name'
+            y='value'
+          />
+          <Lines
+            data={[
+              {Name: 'APPL', value: 50},
+              {Name: 'FB', value: 150},
+              {Name: 'GOOGL', value: 10},
+              {Name: 'APPL2', value: 80},
+              {Name: 'FB2', value: 100},
+              {Name: 'GOOGL2', value: 0},
+            ]}
+            plot={plots.lines}
+            strokeValue='red'
+            x='Name'
+            y='value'
+          />
+        </Chart>
       </ChartMargin>
     </TextBody>
   </Main>
