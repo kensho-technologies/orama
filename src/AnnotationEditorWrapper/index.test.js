c0d2ad6aac3b83bb46f842014ca5eabc59873498
Luis Carli <luiscarli@kensho.com>
2016-01-04 17:38:42 -0500

    package: remove ramda

--- src/AnnotationEditorWrapper/index.test.js
+++ src/AnnotationEditorWrapper/index.test.js
@@ -1,36 +1,36 @@
+//
+// import {it as test} from 'mocha'
+// import assert from 'assert'
+//
+// import React from 'react'
+// import shallowRender from '@luiscarli/shallow-render'
+//
+// import AnnotationEditorWrapper from './'
+//
+// const size = {width: 500, height: 500}
 
-import {it as test} from 'mocha'
-import assert from 'assert'
-
-import React from 'react'
-import shallowRender from '@luiscarli/shallow-render'
-
-import AnnotationEditorWrapper from './'
-
-const size = {width: 500, height: 500}
-
-test('AnnotationEditorWrapper case1', () => {
-  const component = shallowRender(<AnnotationEditorWrapper size={size}/>)
-  assert.deepEqual(component.type, 'noscript')
-})
-
-test('AnnotationEditorWrapper case2', () => {
-  const component = shallowRender(
-    <AnnotationEditorWrapper
-      annotationData={[{}]}
-      size={size}
-    />
-  )
-  assert.deepEqual(component.type, 'noscript')
-})
-
-test('AnnotationEditorWrapper case3', () => {
-  const component2 = shallowRender(
-    <AnnotationEditorWrapper
-      annotationData={[{}]}
-      selectedIdx={0}
-      size={size}
-    />
-  )
-  assert.deepEqual(component2.type.displayName, 'Block')
-})
+// test('AnnotationEditorWrapper case1', () => {
+//   const component = shallowRender(<AnnotationEditorWrapper size={size}/>)
+//   assert.deepEqual(component.type, 'noscript')
+// })
+//
+// test('AnnotationEditorWrapper case2', () => {
+//   const component = shallowRender(
+//     <AnnotationEditorWrapper
+//       annotationData={[{}]}
+//       size={size}
+//     />
+//   )
+//   assert.deepEqual(component.type, 'noscript')
+// })
+//
+// test('AnnotationEditorWrapper case3', () => {
+//   const component2 = shallowRender(
+//     <AnnotationEditorWrapper
+//       annotationData={[{}]}
+//       selectedIdx={0}
+//       size={size}
+//     />
+//   )
+//   assert.deepEqual(component2.type.displayName, 'Block')
+// })
--- src/Explorer/ChartsGroup/index.test.js
+++ src/Explorer/ChartsGroup/index.test.js
@@ -1,32 +1,32 @@
 
-import {it as test} from 'mocha'
-import assert from 'assert'
-import R from 'ramda'
-
-import React from 'react'
-import shallowRender from '@luiscarli/shallow-render'
-
-import ChartsGroup, {getChartElements} from './'
-
-test('ChartsGroup', () => {
-  const component = shallowRender(<ChartsGroup/>)
-  assert.deepEqual(component.type.displayName, 'Flex')
-})
-
-test('getChartElements with empty props', () => {
-  const elements = getChartElements({})
-  assert.ok(
-    elements.props,
-  )
-})
-
-test('getChartElements with some props', () => {
-  const elements = getChartElements({
-    groupProp: 'test',
-    data: [{test: 'a'}, {test: 'a'}, {test: 'a'}],
-  })
-  assert.deepEqual(
-    R.isArrayLike(elements),
-    true
-  )
-})
+// import {it as test} from 'mocha'
+// import assert from 'assert'
+// import R from 'ramda'
+//
+// import React from 'react'
+// import shallowRender from '@luiscarli/shallow-render'
+//
+// import ChartsGroup, {getChartElements} from './'
+//
+// test('ChartsGroup', () => {
+//   const component = shallowRender(<ChartsGroup/>)
+//   assert.deepEqual(component.type.displayName, 'Flex')
+// })
+//
+// test('getChartElements with empty props', () => {
+//   const elements = getChartElements({})
+//   assert.ok(
+//     elements.props,
+//   )
+// })
+//
+// test('getChartElements with some props', () => {
+//   const elements = getChartElements({
+//     groupProp: 'test',
+//     data: [{test: 'a'}, {test: 'a'}, {test: 'a'}],
+//   })
+//   assert.deepEqual(
+//     R.isArrayLike(elements),
+//     true
+//   )
+// })
