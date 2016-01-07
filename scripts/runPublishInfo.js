eb26442665a4be12f4c7fa29753eb035df995411
Luis Carli <luiscarli@kensho.com>
2016-01-07 15:04:52 -0500

    lastPublishInfo: add chores count

--- scripts/runPublishInfo.js
+++ scripts/runPublishInfo.js
@@ -17,12 +17,17 @@ const plotLevel = (commits, level) => {
   console.log(`${c.red(commits.length)} new ${level}`)
   _.each(commits, d => console.log(d))
 }
+const plotChores = chores => {
+  if (_.isEmpty(chores)) return
+  console.log(`${c.red(chores.length)} new chores`)
+}
 
 const formatCommits = (commits) => {
   const fixes = filterCommits(commits, '(FIX)')
   const features = filterCommits(commits, '(FTR)')
   const breaks = filterCommits(commits, '(BRK)')
-  return {fixes, features, breaks}
+  const choresLength = commits.length - fixes.length - features.length - breaks.length
+  return {fixes, features, breaks, chores: _.range(choresLength)}
 }
 
 export const getCommits = clbck => {
@@ -42,11 +47,12 @@ export const getCommits = clbck => {
   })
 }
 
-getCommits(({fixes, features, breaks}, time) => {
+getCommits(({fixes, features, breaks, chores}, time) => {
   console.log('\n')
   console.log(`last published: ${c.yellow(time)}`)
   plotLevel(breaks, 'break change(s)')
   plotLevel(features, 'feature(s)')
   plotLevel(fixes, 'fixe(s)')
+  plotChores(chores)
   console.log('\n')
 })
