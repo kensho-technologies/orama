d5c907ad7ac3bb241c7fada042f98059880693b6
Luis Carli <luiscarli@kensho.com>
2016-01-15 17:29:36 -0500

    mergeDimArrays: tests

---
+++ src/Chart/mergeDimArrays/index.test.js
@@ -0,0 +1,48 @@
+
+import {it as test} from 'mocha'
+import assert from 'assert'
+import {ACCESSORS_GROUPS} from '../../Chart/defaults'
+
+import {omitGroups} from './'
+import {mergeDimArrays} from './'
+
+test('Chart/mergeDimArrays.omitGroups 1', () => {
+  const dimArrays = {
+    fill: [1, 2, 3],
+    x: [1, 2, 3],
+    x0: [1, 2, 3],
+  }
+  const actual = omitGroups(dimArrays, ACCESSORS_GROUPS)
+  const expected = {
+    fill: [1, 2, 3],
+  }
+  assert.deepEqual(actual, expected)
+})
+
+test('Chart/mergeDimArrays.mergeDimArrays 1', () => {
+  const dimArrays = {
+    fill: [1, 2, 3],
+    x: [1, 2],
+    x0: [3, 4],
+  }
+  const actual = mergeDimArrays(dimArrays, ACCESSORS_GROUPS)
+  const expected = {
+    fill: [1, 2, 3],
+    x: [1, 2, 3, 4],
+  }
+  assert.deepEqual(actual, expected)
+})
+test('Chart/mergeDimArrays.mergeDimArrays 2', () => {
+  const dimArrays = {
+    fill: [1, 2, 3],
+    x: [1, 2],
+    x0: undefined,
+    x1: [],
+  }
+  const actual = mergeDimArrays(dimArrays, ACCESSORS_GROUPS)
+  const expected = {
+    fill: [1, 2, 3],
+    x: [1, 2],
+  }
+  assert.deepEqual(actual, expected)
+})
