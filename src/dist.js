cafb861eb3876bba92e13ee450c593da4626c95d
Luis Carli <luiscarli@kensho.com>
2016-02-03 13:20:14 -0500

    dist: tweak exports on the dist file

--- src/dist.js
+++ src/dist.js
@@ -4,11 +4,19 @@ import 'babel-regenerator-runtime'
 import 'es6-promise'
 import 'whatwg-fetch'
 
-export * from './'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-import _ from 'lodash'
+import _ from 'lodash/fp'
+
+export * from './'
+import *  as orama from './'
+import {State} from './devApp/State'
+import * as fetchers from './devApp/fetchers'
+
+global.orama = orama
+global.State = State
+_.each((value, key) => global[key] = value, orama)
+_.each((value, key) => global[key] = value, fetchers)
 
 global.React = React
 global.ReactDOM = ReactDOM
