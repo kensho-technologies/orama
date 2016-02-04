3f5ce5a1ce2e114cd4899fc8c3e2a2293b0c2646
Luis Carli <luiscarli@kensho.com>
2016-02-03 19:48:28 -0500

    fetchers: add fetchQuandl

--- src/devApp/fetchers/index.js
+++ src/devApp/fetchers/index.js
@@ -1,8 +1,31 @@
+/* eslint no-param-reassign:0 */
+
+import {flow, get, map, zipObject, each, sortBy} from 'lodash/fp'
+import {timeParse} from 'd3-time-format'
+
+const getDate = timeParse('%Y-%m-%d')
+
+const quandlMap = flow(
+  get('dataset_data.column_names'),
+  zipObject,
+)
+
+const quandlFlow = (result, name) =>
+  flow(
+    get('dataset_data.data'),
+    map(quandlMap(result)),
+    each(d => {
+      d.Name = name
+      d.Date = getDate(d.Date)
+    }),
+    sortBy('Date'),
+  )(result)
 
 export const fetchJson = url =>
-  fetch(url)
-    .then(response => response.json())
+  fetch(url).then(r => r.json())
 
 export const fetchText = url =>
-  fetch(url)
-    .then(response => response.text())
+  fetch(url).then(r => r.text())
+
+export const fetchQuandl = (url, name) =>
+  fetchJson(url).then(r => quandlFlow(r, name))
