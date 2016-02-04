c3eea6445472815cc5b90be5ed57c4e9d3b19846
Luis Carli <luiscarli@kensho.com>
2016-02-04 15:54:37 -0500

    getHtmlCode: wrap code in html tags

---
+++ src/devApp/ExamplesViewer/getHtmlCode/index.js
@@ -0,0 +1,12 @@
+
+export const getHtmlCode = code => `<!DOCTYPE html>
+<meta charset="utf-8">
+<script src="https://npmcdn.com/orama/dist/orama.min.js"></script>
+<script src="https://npmcdn.com/babel-core@5.8.34/browser.min.js"></script>
+<div id="root"/>
+<script type="text/babel">
+
+${code}
+
+ReactDOM.render(<DataVis/>, document.getElementById('root'))
+</script>`
