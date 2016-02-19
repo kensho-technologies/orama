160072999c3279a7b9d4b0550b9338018863dcfa
Luis Carli <luiscarli@kensho.com>
2016-02-19 12:49:34 -0500

    scripts/example: script for bootstrapping new example

---
+++ scripts/example.js
@@ -0,0 +1,51 @@
+
+const _ = require('lodash/fp')
+const fs = require('fs')
+
+const title = process.argv[2] || 'Example'
+const name = _.upperFirst(_.camelCase(title))
+
+const file = `
+/* eslint react/prop-types:0 */
+
+export const title = '${title}'
+export const tags = []
+export const hide = false
+export const date = new Date('${new Date()}')
+export const description = \`\`
+export code from '!!raw!./'
+
+import React from 'react'
+import {Chart, Lines} from '../../../'
+import {State} from 'on-update'
+
+export const Component = props =>
+  <Chart>
+    <Lines
+      data={props.fbData}
+      x='Date'
+      y='Adj. Close'
+    />
+  </Chart>
+
+const startWith = props => {
+  props.setState({})
+}
+
+export const DataVis = props =>
+  <State startWith={startWith}>
+    <Component {...props}/>
+  </State>
+`
+
+fs.mkdirSync(`./src/devApp/examples/${name}/`)
+fs.writeFileSync(`./src/devApp/examples/${name}/index.js`, file)
+
+const examples = _.trim(
+  fs.readFileSync('./src/devApp/examples/index.js', 'utf8')
+)
+
+fs.writeFileSync('./src/devApp/examples/index.js', `
+${examples}
+export * as ${name} from './${name}'
+`)
