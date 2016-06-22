585177c0feb302e18613ad9861fdbb524d33afe3
Luis Carli <luiscarli@kensho.com>
2016-06-21 21:43:28 -0400

    examples/HighlightExtension: update

--- src/devApp/examples/HighlightExtension/index.js
+++ src/devApp/examples/HighlightExtension/index.js
@@ -1,18 +1,27 @@
 
 export const title = 'Highlight Extension'
 export const tags = []
-export const hide = true
+export const hide = false
 export const date = new Date('Thu Mar 10 2016 13:37:11 GMT-0500 (EST)')
-export const description = ''
+export const description = `The onUpdate prop can be used for the parent to handle the highlight data directly, instead of on the state inside the Highlight component.
+
+When the highlight data is not handled by the component, it needs to receive it as a prop.
+
+Slice is an optional prop that define the index in which the Highlight will be inserted on the Chart.
+
+The Highlight component is a thin wrapper around the Chart onUpdate api, in more complex scenarios it may be better to directly handling the onUpdate from the Chart instead of through the Hightlight component.`
+
 export code from '!!raw!./'
 
 import React from 'react'
 import {Chart, Points, Highlight} from '../../../'
-import {State} from 'on-update'
 
-export const Component = props =>
+
+export const DataVis = props =>
   <Highlight
     // onUpdate={(d)=>console.log(d)}
+    // data={[]}
+    // slice={1}
     Component={Points}
     componentProps={{
       x: 'Volume',
@@ -31,12 +40,3 @@ export const Component = props =>
       />
     </Chart>
   </Highlight>
-
-const startWith = props => {
-  props.setState({})
-}
-
-export const DataVis = props =>
-  <State startWith={startWith}>
-    <Component {...props}/>
-  </State>
