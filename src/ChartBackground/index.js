1ea8d6762ed7d7d7aceba087f6fdc49f579a06e0
Billy Janitsch <billyjanitsch@gmail.com>
2018-06-08 16:39:44 -0400

    Refactor ChartBackground (#134)
    * Refactor ChartBackground
    
    * Compute text labels inline

--- src/ChartBackground/index.js
+++ src/ChartBackground/index.js
@@ -14,149 +14,161 @@ import basicRender from '../canvas/basicRender'
 import BottomLabel from './BottomLabel'
 import LeftLabel from './LeftLabel'
 
-export function getBackground(props) {
-  if (props.backgroundShow === false) return undefined
-  const {backgroundOffset, plotRect, theme} = props
-  const backgroundRect = inset(-backgroundOffset, plotRect)
-  const backgroundPath = getPath2D()
-  backgroundPath.rect(
-    backgroundRect.x,
-    backgroundRect.y,
-    backgroundRect.width,
-    backgroundRect.height
-  )
-  return {
-    fill: theme.plotBackgroundFill,
-    path2D: backgroundPath,
-    stroke: 'transparent',
-    type: 'area',
+export default class ChartBackground extends React.Component {
+  static propTypes = {
+    backgroundShow: PropTypes.bool,
+    backgroundOffset: PropTypes.number.isRequired,
+    groupedKeys: PropTypes.arrayOf(PropTypes.string),
+    height: PropTypes.number,
+    layers: PropTypes.arrayOf(PropTypes.object),
+    plotRect: CustomPropTypes.plotRect.isRequired,
+    theme: CustomPropTypes.theme.isRequired,
+    width: PropTypes.number,
+    x: PropTypes.string,
+    xName: PropTypes.string,
+    xScale: PropTypes.func,
+    xShowGuides: PropTypes.bool,
+    xShowLabel: PropTypes.bool.isRequired,
+    xShowTicks: PropTypes.bool,
+    xTicks: PropTypes.arrayOf(PropTypes.object),
+    xTickOffset: PropTypes.number,
+    y: PropTypes.string,
+    yName: PropTypes.string,
+    yScale: PropTypes.func,
+    yShowGuides: PropTypes.bool,
+    yShowLabel: PropTypes.bool.isRequired,
+    yShowTicks: PropTypes.bool,
+    yTicks: PropTypes.arrayOf(PropTypes.object),
+    yTickOffset: PropTypes.number,
   }
-}
 
-export function getXGuides(props, thick) {
-  if (!includes(props.groupedKeys, 'x')) return undefined
-  if (props.xShowGuides === false) return undefined
-  const {backgroundOffset, plotRect, theme, xScale, xTicks} = props
-  return map(xTicks, d => {
-    const linePath = getPath2D()
-    const x = xScale(d.value)
-    linePath.moveTo(x, plotRect.y - backgroundOffset)
-    linePath.lineTo(x, plotRect.y + plotRect.height + backgroundOffset)
+  getBackground() {
+    const {backgroundShow, backgroundOffset, plotRect, theme} = this.props
+    if (backgroundShow === false) return undefined
+    const {height, width, x, y} = inset(-backgroundOffset, plotRect)
+    const path2D = getPath2D()
+    path2D.rect(x, y, width, height)
     return {
-      path2D: linePath,
-      type: 'line',
-      lineWidth: thick ? theme.guideZeroLineWidth : theme.guideLineWidth,
-      stroke: thick ? theme.guideZeroStroke : theme.guideStroke,
+      fill: theme.plotBackgroundFill,
+      path2D,
+      stroke: 'transparent',
+      type: 'area',
     }
-  })
-}
+  }
 
-export function getYGuides(props, thick) {
-  if (!includes(props.groupedKeys, 'y')) return undefined
-  if (props.yShowGuides === false) return undefined
-  const {backgroundOffset, plotRect, theme, yScale, yTicks} = props
-  return map(yTicks, d => {
-    const linePath = getPath2D()
-    const y = yScale(d.value)
-    linePath.moveTo(plotRect.x - backgroundOffset, y)
-    linePath.lineTo(plotRect.x + plotRect.width + backgroundOffset, y)
-    return {
-      path2D: linePath,
-      type: 'line',
-      lineWidth: thick ? theme.guideZeroLineWidth : theme.guideLineWidth,
-      stroke: thick ? theme.guideZeroStroke : theme.guideStroke,
-    }
-  })
-}
+  getXGuides(xTicks, thick) {
+    const {backgroundOffset, groupedKeys, plotRect, theme, xScale, xShowGuides} = this.props
+    if (!includes(groupedKeys, 'x')) return undefined
+    if (xShowGuides === false) return undefined
+    return map(xTicks, d => {
+      const path2D = getPath2D()
+      const x = xScale(d.value)
+      path2D.moveTo(x, plotRect.y - backgroundOffset)
+      path2D.lineTo(x, plotRect.y + plotRect.height + backgroundOffset)
+      return {
+        path2D,
+        type: 'line',
+        lineWidth: thick ? theme.guideZeroLineWidth : theme.guideLineWidth,
+        stroke: thick ? theme.guideZeroStroke : theme.guideStroke,
+      }
+    })
+  }
 
-export function getXText(props) {
-  if (!includes(props.groupedKeys, 'x')) return undefined
-  if (props.xShowTicks === false) return undefined
-  const {theme} = props
-  const defaultOffset = theme.axisTickFontSize * (theme.lineHeight - 1)
-  const {backgroundOffset, plotRect, xScale, xTickOffset = defaultOffset, xTicks} = props
-  return map(xTicks, d => ({
-    type: 'text',
-    text: d.text,
-    x: xScale(d.value),
-    y: sum([backgroundOffset, plotRect.y, plotRect.height, xTickOffset]),
-    textBaseline: 'top',
-    textAlign: 'center',
-    font: `${theme.axisTickFontSize}px ${theme.fontFamilyMono}`,
-  }))
-}
+  getYGuides(yTicks, thick) {
+    const {backgroundOffset, groupedKeys, plotRect, theme, yScale, yShowGuides} = this.props
+    if (!includes(groupedKeys, 'y')) return undefined
+    if (yShowGuides === false) return undefined
+    return map(yTicks, d => {
+      const path2D = getPath2D()
+      const y = yScale(d.value)
+      path2D.moveTo(plotRect.x - backgroundOffset, y)
+      path2D.lineTo(plotRect.x + plotRect.width + backgroundOffset, y)
+      return {
+        path2D,
+        type: 'line',
+        lineWidth: thick ? theme.guideZeroLineWidth : theme.guideLineWidth,
+        stroke: thick ? theme.guideZeroStroke : theme.guideStroke,
+      }
+    })
+  }
 
-export function getYText(props) {
-  if (!includes(props.groupedKeys, 'y')) return undefined
-  if (props.yShowTicks === false) return undefined
-  const {theme} = props
-  const defaultOffset = theme.axisTickFontSize * (theme.lineHeight - 1)
-  const {backgroundOffset, plotRect, yScale, yTickOffset = defaultOffset, yTicks} = props
-  return map(yTicks, d => ({
-    type: 'text',
-    text: d.text,
-    x: sum([plotRect.x, -backgroundOffset, -yTickOffset]),
-    y: yScale(d.value),
-    textAlign: 'right',
-    textBaseline: 'middle',
-    fill: theme.textFill,
-    font: `${theme.axisTickFontSize}px ${theme.fontFamilyMono}`,
-  }))
-}
+  getXText(xTicks) {
+    const {groupedKeys, theme, xShowTicks} = this.props
+    if (!includes(groupedKeys, 'x')) return undefined
+    if (xShowTicks === false) return undefined
+    const defaultOffset = theme.axisTickFontSize * (theme.lineHeight - 1)
+    const {backgroundOffset, plotRect, xScale, xTickOffset = defaultOffset} = this.props
+    return map(xTicks, d => ({
+      type: 'text',
+      text: d.text,
+      x: xScale(d.value),
+      y: sum([backgroundOffset, plotRect.y, plotRect.height, xTickOffset]),
+      textBaseline: 'top',
+      textAlign: 'center',
+      font: `${theme.axisTickFontSize}px ${theme.fontFamilyMono}`,
+    }))
+  }
 
-export function getBackgroundRenderData(props) {
-  const background = getBackground(props)
-  const xTicks = props.xTicks || getTicks(props, 'x')
-  const yTicks = props.yTicks || getTicks(props, 'y')
-  const xGuides = getXGuides({...props, xTicks})
-  const yGuides = getYGuides({...props, yTicks})
-  const xText = getXText({...props, xTicks})
-  const yText = getYText({...props, yTicks})
-  const thickXGuide = getXGuides({...props, xTicks: filter(xTicks, d => d.value === 0)}, true)
-  const thickYGuide = getYGuides({...props, yTicks: filter(yTicks, d => d.value === 0)}, true)
-  return flatten(compact([background, xGuides, yGuides, thickXGuide, thickYGuide, xText, yText]))
-}
+  getYText(yTicks) {
+    const {groupedKeys, theme, yShowTicks} = this.props
+    if (!includes(groupedKeys, 'y')) return undefined
+    if (yShowTicks === false) return undefined
+    const defaultOffset = theme.axisTickFontSize * (theme.lineHeight - 1)
+    const {backgroundOffset, plotRect, yScale, yTickOffset = defaultOffset} = this.props
+    return map(yTicks, d => ({
+      type: 'text',
+      text: d.text,
+      x: sum([plotRect.x, -backgroundOffset, -yTickOffset]),
+      y: yScale(d.value),
+      textAlign: 'right',
+      textBaseline: 'middle',
+      fill: theme.textFill,
+      font: `${theme.axisTickFontSize}px ${theme.fontFamilyMono}`,
+    }))
+  }
 
-function getLabelText(props, key) {
-  const text = props[`${key}Name`] || props[key]
-  if (text) return text
-  const layer = findLast(props.layers, d => d[`${key}Name`] || d[key])
-  if (layer) return layer[`${key}Name`] || layer[key]
-  return undefined
-}
+  getBackgroundRenderData() {
+    const background = this.getBackground()
+    const xTicks = this.props.xTicks || getTicks(this.props, 'x')
+    const yTicks = this.props.yTicks || getTicks(this.props, 'y')
+    const xGuides = this.getXGuides(xTicks)
+    const yGuides = this.getYGuides(yTicks)
+    const xText = this.getXText(xTicks)
+    const yText = this.getYText(yTicks)
+    const thickXGuide = this.getXGuides(filter(xTicks, d => d.value === 0), true)
+    const thickYGuide = this.getYGuides(filter(yTicks, d => d.value === 0), true)
+    return flatten(compact([background, xGuides, yGuides, thickXGuide, thickYGuide, xText, yText]))
+  }
 
-export default function ChartBackground(props) {
-  const {height, plotRect, theme, width, xShowLabel, yShowLabel} = props
-  return (
-    <React.Fragment>
-      <Canvas
-        height={height}
-        plotRect={plotRect}
-        render={basicRender}
-        renderData={getBackgroundRenderData(props)}
-        theme={theme}
-        width={width}
-      />
-      {yShowLabel && (
-        <LeftLabel plotRect={plotRect} text={getLabelText(props, 'y')} theme={theme} />
-      )}
-      {xShowLabel && (
-        <BottomLabel plotRect={plotRect} text={getLabelText(props, 'x')} theme={theme} />
-      )}
-    </React.Fragment>
-  )
-}
+  getLabelText(key) {
+    const {layers} = this.props
+    const keyName = `${key}Name`
+    const text = this.props[keyName] || this.props[key]
+    if (text) return text
+    const layer = findLast(layers, d => d[keyName] || d[key])
+    if (layer) return layer[keyName] || layer[key]
+    return undefined
+  }
 
-ChartBackground.propTypes = {
-  height: PropTypes.number,
-  onUpdate: PropTypes.func,
-  plotRect: CustomPropTypes.plotRect.isRequired,
-  theme: CustomPropTypes.theme.isRequired,
-  width: PropTypes.number,
-  x: PropTypes.string,
-  xName: PropTypes.string,
-  xShowLabel: PropTypes.bool,
-  y: PropTypes.string,
-  yName: PropTypes.string,
-  yShowLabel: PropTypes.bool,
+  render() {
+    const {height, plotRect, theme, width, xShowLabel, yShowLabel} = this.props
+    return (
+      <React.Fragment>
+        <Canvas
+          height={height}
+          plotRect={plotRect}
+          render={basicRender}
+          renderData={this.getBackgroundRenderData()}
+          theme={theme}
+          width={width}
+        />
+        {yShowLabel && (
+          <LeftLabel plotRect={plotRect} text={this.getLabelText('y')} theme={theme} />
+        )}
+        {xShowLabel && (
+          <BottomLabel plotRect={plotRect} text={this.getLabelText('x')} theme={theme} />
+        )}
+      </React.Fragment>
+    )
+  }
 }
--- test/ChartBackground/index.js
+++ test/ChartBackground/index.js
@@ -6,79 +6,10 @@ import {it as test} from 'mocha'
 import * as React from 'react'
 
 import shallowRender from '../helpers/shallowRender'
-import {THEME as theme} from '../../src/defaults'
-import ChartBackground, {
-  getBackground,
-  getXGuides,
-  getYGuides,
-  getXText,
-  getYText,
-  getBackgroundRenderData,
-} from '../../src/ChartBackground'
+import ChartBackground from '../../src/ChartBackground'
 
 const plotRect = {x: 10, y: 10, width: 480, height: 480}
 
 test('ChartBackground', () => {
-  assert(shallowRender(<ChartBackground height={50} plotRect={{}} theme={theme} width={50} />))
-})
-
-test('ChartBackground.getBackground', () => {
-  const props = {theme}
-  const background = getBackground(props)
-  assert.deepEqual(background.type, 'area')
-  assert.deepEqual(background.fill, theme.plotBackgroundFill)
-})
-
-test('ChartBackground.getXGuides', () => {
-  const props = {
-    theme,
-    groupedKeys: ['x'],
-    xTicks: [{value: 10}],
-    xScale: d => d,
-    plotRect,
-  }
-  const xGuides = getXGuides(props)
-  assert.deepEqual(xGuides[0].type, 'line')
-})
-
-test('ChartBackground.getYGuides', () => {
-  const props = {
-    theme,
-    groupedKeys: ['y'],
-    yTicks: [{value: 10}],
-    yScale: d => d,
-    plotRect,
-  }
-  const yGuides = getYGuides(props)
-  assert.deepEqual(yGuides[0].type, 'line')
-})
-
-test('ChartBackground.getXText', () => {
-  const props = {
-    theme,
-    groupedKeys: ['x'],
-    xTicks: [{value: 10}],
-    xScale: d => d,
-    plotRect,
-  }
-  const xGuides = getXText(props)
-  assert.deepEqual(xGuides[0].type, 'text')
-})
-
-test('ChartBackground.getYText', () => {
-  const props = {
-    theme,
-    groupedKeys: ['y'],
-    yTicks: [{value: 10}],
-    yScale: d => d,
-    plotRect,
-  }
-  const yGuides = getYText(props)
-  assert.deepEqual(yGuides[0].type, 'text')
-})
-
-test('ChartBackground.getBackgroundRenderData', () => {
-  const props = {theme}
-  const backgroundRenderData = getBackgroundRenderData(props)
-  assert.deepEqual(backgroundRenderData[0].type, 'area')
+  assert(shallowRender(<ChartBackground height={50} plotRect={plotRect} theme={{}} width={50} />))
 })
